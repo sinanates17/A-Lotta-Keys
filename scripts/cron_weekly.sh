05a1ffa55eccdbe0cc292bd3fbfa2a74d1e94950
Hugged <sinanates17@gmail.com>
2025-05-18 13:26:29 -0400

    A commit

--- scripts/cron_weekly.sh
+++ scripts/cron_weekly.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+PATH_THIS="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$PATH_THIS"
+cd ..
+
 set -o allexport
-source ../.env
+source .env
 set +o allexport
 
 source "$PATH_VENV/bin/activate"
+
+python3 cron_daily.py
 
 python3 cron_weekly.py
