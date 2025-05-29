e405551cb91e8ec0052652f421022f908c3b6592
Hugged <sinanates17@gmail.com>
2025-05-29 13:15:59 -0400

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,14 +1,4 @@
-import os
-from dotenv import load_dotenv
+from app import app
 
-try:
-    load_dotenv("/home/sinanates/Github/A-Lotta-Keys/.env")
-except Exception as e:
-    with open("/tmp/wsgi_error.log", "w") as f:
-        f.write(f"Failed to load .env: {e}\n")
-
-try:
-    from app import app as application
-except Exception as e:
-    with open("/tmp/wsgi_error.log", "a") as f:
-        f.write(f"Failed to load Flask app: {e}\n")
+application = app
+#For production
