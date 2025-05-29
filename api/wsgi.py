202798e63f61420cb076a15df14f808d0d56837d
Hugged <sinanates17@gmail.com>
2025-05-29 11:52:44 -0400

    wsgi

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,6 +1,8 @@
 from app import app
 from dotenv import load_dotenv
+import sys
+from pathlib import Path; sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
-load_dotenv("../.env")
+load_dotenv()
 application = app
 #For production
