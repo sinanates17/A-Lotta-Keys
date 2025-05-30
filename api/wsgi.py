f84f3ebec09d217776abbc970f5ab426ac30e509
Hugged <sinanates17@gmail.com>
2025-05-30 11:57:44 -0400

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,10 +1,4 @@
-def application(environ, start_response):
-    with open('/tmp/wsgi_env.log', 'w') as f:
-        for key, value in environ.items():
-            f.write(f"{key}: {value}\n")
-    
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b"WSGI environment dumped.\n"]
-application()
+import sys
+print(sys.path, file=sys.stderr)
 
 from app import app as application
