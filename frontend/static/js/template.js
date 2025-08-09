04d69d89e3b18d0aeca1d4c2084bd7534028da8c
Hugged <sinanates17@gmail.com>
2025-08-08 23:55:14 -0400

    Web design adjustments

--- frontend/static/js/template.js
+++ frontend/static/js/template.js
@@ -2,4 +2,21 @@ const isDev = window.location.hostname === "127.0.0.1";
 
 API_BASE = isDev
   ? "http://127.0.0.1:5000/"
-  : "https://alottakeys.xyz/";
+  : "http://alottakeys.xyz/";
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const logout = document.getElementById("logout")
+    const avataricon = document.getElementById("avataricon")
+    const login = document.getElementById("login")
+
+    if (logout) {
+        logout.onclick = () => { window.location.href = `${API_BASE}auth/logout` }
+        avataricon.onclick = () => { window.location.href = `${API_BASE}api/search/users/{{ id }}` }
+    }
+
+    else {
+        login.onclick = () => { window.location.href = `${API_BASE}auth/login` }
+    }
+
+})
